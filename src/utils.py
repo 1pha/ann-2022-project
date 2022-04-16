8252fee61246055086aef2ad79fb7e25e3576911
1pha <1phantasmas@korea.ac.kr>
2022-04-16 21:14:11 +0900

    <utils> add get_today

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+from datetime import datetime as dt
+
+def get_today():
+
+    td = dt.today()
+
+    return (
+        str(td.year)
+        + str(td.month).zfill(2)
+        + str(td.day).zfill(2)
+        + "-"
+        + str(td.hour).zfill(2)
+        + str(td.minute).zfill(2)
+    )
