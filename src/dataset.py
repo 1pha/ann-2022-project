98d303148448d4fb8b0dc6469395d09c3123e293
1pha <1phantasmas@korea.ac.kr>
2022-04-16 18:17:07 +0900

    <dataset> add augmentation

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,8 @@
 import pathlib
 import tensorflow as tf
+from tensorflow.keras import layers
+
+AUTO = tf.data.AUTOTUNE
 
 
 def load_train_dataset(
@@ -8,6 +11,8 @@ def load_train_dataset(
     img_height: int = 224,
     img_width: int = 224,
     batch_size: int = 16,
+    randaugment_n: int = 3,
+    randaugment_m: int = 2,
 ):
     data_dir = pathlib.Path(path)
 
@@ -20,6 +25,7 @@ def load_train_dataset(
         batch_size=batch_size,
     )
     train_ds = _apply_normalization(train_ds)
+    train_ds = _apply_augmentation(train_ds, n=randaugment_n, m=randaugment_m)
 
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
@@ -30,15 +36,36 @@ def load_train_dataset(
         batch_size=batch_size,
     )
     val_ds = _apply_normalization(val_ds)
+    val_ds = _apply_augmentation(val_ds, n=randaugment_n, m=randaugment_m)
 
-    return train_ds, val_ds
+    return train_ds.prefetch(AUTO), val_ds.prefetch(AUTO)
+
 
 def _apply_normalization(dataset):
 
-    normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+    normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(
+        1.0 / 255
+    )
     normalized_ds = dataset.map(lambda x, y: (normalization_layer(x), y))
     return normalized_ds
 
-if __name__=="__main__":
+
+def _apply_augmentation(dataset, n=3, m=2, seed: int = 42):
+
+    simple_aug = tf.keras.Sequential(
+        [
+            layers.RandomFlip(),
+            layers.RandomRotation(factor=0.02),
+            layers.RandomZoom(height_factor=0.2, width_factor=0.2),
+        ]
+    )
+
+    augmentation_ds = dataset.map(
+        lambda x, y: (simple_aug(x), y), num_parallel_calls=AUTO
+    )
+    return augmentation_ds
+
+
+if __name__ == "__main__":
 
     train_ds, val_ds = load_train_dataset()
