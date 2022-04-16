b3258ee0a7cc8fe4a30f14631f40e1df26f6a561
1pha <1phantasmas@korea.ac.kr>
2022-04-16 18:18:38 +0900

    <pkg> make src as a package

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 from .model import build_googlenet
 from .dataset import load_train_dataset
+from .evaluate import calculate_result
+from .config import Configuration
 
-__all__ = ["build_googlenet", "load_train_dataset"]
+__all__ = ["build_googlenet", "load_train_dataset", "calculate_result", "Configuration"]
