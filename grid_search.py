4b694b92f33d53541a96446eeba9ccf3e2a4a67e
1pha <1phantasmas@korea.ac.kr>
2022-04-20 20:13:29 +0900

    <formatting> black, isort

--- grid_search.py
+++ grid_search.py
@@ -11,14 +11,13 @@ for learning_rate in [1e-2, 1e-3]:
                     for scheduler in ["plateau", None]:
                         config = Configuration()
                         tf.random.set_seed(config.seed)
-                        
+
                         config.learning_rate = learning_rate
                         config.batch_size = batch_size
                         config.use_augmentation = use_augmentation
                         config.regularizer = regularizer
                         config.label_smoothing = label_smoothing
                         config.scheduler = scheduler
-                        
 
                         config.output_dir = f"./models/LR-{learning_rate}_BSZ-{batch_size}_AUG-{use_augmentation}_REG-{regularizer}_LS-{label_smoothing}_SCH-{scheduler}"
                         os.makedirs(config.output_dir, exist_ok=True)
