08535e79240a324e56c78a555b75181f98d8cdb9
1pha <1phantasmas@korea.ac.kr>
2022-04-22 00:35:31 +0900

    <update> create batch

--- grid_search.py
+++ grid_search.py
@@ -3,27 +3,29 @@ import tensorflow as tf
 
 from src import run, Configuration
 
-for learning_rate in [1e-2, 1e-3]:
-    for batch_size in [16, 32]:
-        for use_augmentation in [True, False]:
-            for regularizer in ["l2", None]:
-                for label_smoothing in [0.3, 0]:
-                    for scheduler in ["plateau", None]:
-                        config = Configuration()
-                        tf.random.set_seed(config.seed)
+for seed in [42, 43, 44, 45, 46]:
+    for augmentation in [False, "soft", "hard"]:
+        for pre_trained in [False, "freeze", "fine"]:
 
-                        config.learning_rate = learning_rate
-                        config.batch_size = batch_size
-                        config.use_augmentation = use_augmentation
-                        config.regularizer = regularizer
-                        config.label_smoothing = label_smoothing
-                        config.scheduler = scheduler
+            config = Configuration()
+            config.seed = seed
+            config.use_augmentation = augmentation
+            if pre_trained == False:
+                config.use_pretrained = False
+                config.linear_probing = False
+                
+            elif pre_trained == "freeze":
+                config.use_pretrained = True
+                config.linear_proibing = False
+                
+            elif pre_trained == "fine":
+                config.use_pretrained = True
+                config.linear_proibing = True
+                
+            tf.random.set_seed(config.seed)
 
-                        config.output_dir = f"./models/LR-{learning_rate}_BSZ-{batch_size}_AUG-{use_augmentation}_REG-{regularizer}_LS-{label_smoothing}_SCH-{scheduler}"
-                        os.makedirs(config.output_dir, exist_ok=True)
-                        print(config.output_dir)
-                        try:
-                            tf.keras.backend.clear_session()
-                            run(config)
-                        except:
-                            raise
+            try:
+                tf.keras.backend.clear_session()
+                run(config)
+            except:
+                raise
