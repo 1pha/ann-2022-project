4e3b995b81ef6dcdd17279cb0915cc70fbb649ba
1pha <1phantasmas@korea.ac.kr>
2022-05-06 00:12:17 +0900

    <update> assignment 2

--- src/xai.py
+++ src/xai.py
@@ -1,61 +1,53 @@
-import keras
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import preprocess_input, decode_predictions
-from keras.preprocessing import image
-import keras.backend as K
-
-import tensorflow as tf
-from tensorflow.python.framework import ops
-
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
-from utils import deprocess_image
+@tf.custom_gradient
+def guidedRelu(x):
+  def grad(dy):
+    return tf.cast(dy>0,"float32") * tf.cast(x>0, "float32") * dy
+  return tf.nn.relu(x), grad
 
+def visualize(model, input_img, layer_name):
 
-def load_image(path, target_size=(224, 224)):
-    x = image.load_img(path, target_size=target_size)
-    x = image.img_to_array(x)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+  gb_model = tf.keras.models.Model(
+      inputs = [model.inputs],    
+      outputs = [model.get_layer(layer_name).output]
+  )
+  layer_dict = [layer for layer in gb_model.layers[1:] if hasattr(layer,'activation')]
 
-    return x
+  for layer in layer_dict:
+    if layer.activation == tf.keras.activations.relu:
+      layer.activation = guidedRelu
 
+  with tf.GradientTape() as tape:
+      inputs = tf.expand_dims(input_img, axis=0)
+      tape.watch(inputs)
+      outputs = gb_model(inputs)[0]
+  grads = tape.gradient(outputs, inputs)[0]
 
-def register_gradient():
-    if "GuidedBackProp" not in ops._gradient_registry._registry:
-        @ops.RegisterGradient("GuidedBackProp")
-        def _GuidedBackProp(op, grad):
-            dtype = op.inputs[0].dtype
-            return grad * tf.cast(grad > 0., dtype) * \
-                tf.cast(op.inputs[0] > 0., dtype)
+  # Calculate GradCAM
+  weights = tf.reduce_mean(grads, axis=(0, 1))
+  grad_cam = np.ones(outputs.shape[0: 2], dtype = np.float32)
+  for i, w in enumerate(weights):
+      grad_cam += w * outputs[:, :, i]
 
+  # GradCAM
+  grad_cam_img = cv2.resize(grad_cam.numpy(), (224, 224))
+  grad_cam_img = np.maximum(grad_cam_img, 0)
+  heatmap = (grad_cam_img - grad_cam_img.min()) / (grad_cam_img.max() - grad_cam_img.min())
+  grad_cam_img = cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET)
+  output_image = cv2.addWeighted(cv2.cvtColor(input_img.numpy().astype('uint8'), cv2.COLOR_RGB2BGR), 0.5, grad_cam_img, 1, 0)
 
-def modify_backprop(model, name):
-    g = tf.get_default_graph()
-    with g.gradient_override_map({'Relu': name}):
+  # Guided Backpropagation
+  guided_back_prop = grads
+  gb_viz = np.dstack((
+              guided_back_prop[:, :, 0],
+              guided_back_prop[:, :, 1],
+              guided_back_prop[:, :, 2],
+          ))       
+  gb_viz -= np.min(gb_viz)
+  gb_viz /= gb_viz.max()
 
-        # get layers that have an activation
-        layer_dict = [layer for layer in model.layers[1:]
-                      if hasattr(layer, 'activation')]
-
-        # replace relu activation
-        for layer in layer_dict:
-            if layer.activation == keras.activations.relu:
-                layer.activation = tf.nn.relu
-
-        # re-instanciate a new model
-        new_model = VGG16(weights='imagenet')
-    return new_model
-
-
-def guided_backpropagation(img_tensor, model, activation_layer):
-    model_input = model.input
-    layer_output = model.get_layer(activation_layer).output
-
-    max_output = K.max(layer_output, axis=3)
-
-    get_output = K.function([model_input], [K.gradients(max_output, model_input)[0]])
-    saliency = get_output([img_tensor])
-
-    return saliency[0]
+  return output_image, gb_viz
